9541308166d31e48ab2a99e09219c73b37dd519b
miwarn <miwarn-git@outlook.de>
2022-08-24 10:11:29 +0200

    new interfaces

--- segments.ts
+++ segments.ts
@@ -1,1 +1,22 @@
-/* Segment Structures */
+type StructureState = 'M'|'C'
+
+interface ElementStructure {
+    tag: string,
+    name: string,
+    state: StructureState,
+    format: string
+}
+
+interface CompositeStructure {
+    tag: string,
+    name: string,
+    state: StructureState,
+    elements: ElementStructure[]
+}
+
+interface SegmentData {
+    tag: string,
+    data: Array<string|string[]>
+}
+
+type SegmentList = SegmentDats[]
